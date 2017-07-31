698379e4b37c11ad911bf680e8723d56985459e4
Asia Gault <asialevesquegault@gmail.com>
2017-07-31 23:55:47 +0000

    download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,27 +1,53 @@
-
+//downloadImageByURL(process.argv.slice(2)
 var request = require('request');
 
-var options = {
-  url: 'https://api.github.com/repos/jquery/jquery/contributors',
-  headers: {
-    'User-Agent': 'Bacon'
-  }
-};
-request(options, function(err, response, body) {
-  if (err) throw err;
-  console.log('Response Status Code:', response.statusCode);
-});
+
 
 var GITHUB_USER = "agault";
 var GITHUB_TOKEN = "a51410f8cd2ebcb0b1884aa1d30cc200a3156548";
 
+
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-console.log(requestURL);
+    var options = {
+       url: requestURL,
+        headers: {
+          'User-Agent': 'Bacon'
+         }
+ };
+ request(options, function(err, response, body) {
+   if (err) throw err;
+   //console.log('Response Status Code:', body);
+     var parsebody = JSON.parse(body);
+     //console.log(parsebody)
+     cb(err, parsebody)
+ });
 }
 
+getRepoContributors("jquery", "jquery", function(err, result) {
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
+  result.forEach(function (user){
+    var filePath = "./avatars/" + user.login + ".png"
+    downloadImageByURL(user.avatar_url, filePath)
+  })
+});
 
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//   console.log("Errors:", err);
-//   console.log("Result:", result);
-// });
+function downloadImageByURL(url, filePath) {
+  var request = require('request');
+  var fs = require('fs');
+
+  request.get(url)
+    .on('error', function (err){
+      throw err;
+    })
+    .on('response', function (response) {                           // Note 3
+      console.log('Response Status Code: ', response.statusCode);
+    })
+    .pipe(fs.createWriteStream(filePath));               // Note 4
+}
+//console.log(downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg"))
+
+// downloadImageByURL()
