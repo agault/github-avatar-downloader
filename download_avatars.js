fecf4aedefaa8756c232e7d7cb9710a15771a415
Asia Gault <asialevesquegault@gmail.com>
2017-07-31 21:09:30 +0000

    download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+var GITHUB_USER = "agault";
+var GITHUB_TOKEN = "a51410f8cd2ebcb0b1884aa1d30cc200a3156548";
